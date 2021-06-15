f6e7f118276be1586144e206e41dc31d9977ea14
Piyush Kumar Prusty <piyushprusty10@gmail.com>
2021-06-15 18:33:55 +0530

    automatically audio will be played.

--- app/src/main/java/com/example/android/mediaplayer/MainActivity.java
+++ app/src/main/java/com/example/android/mediaplayer/MainActivity.java
@@ -2,13 +2,36 @@ package com.example.android.mediaplayer;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.media.MediaPlayer;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.SeekBar;
 
 public class MainActivity extends AppCompatActivity {
+    private Button play;
+    private Button pause;
+    private SeekBar seekBar;
+    private MediaPlayer mediaPlayer;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mediaPlayer = MediaPlayer.create(this, R.raw.music);
+        mediaPlayer.start();
+
+        play.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+        pause.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
     }
 }
