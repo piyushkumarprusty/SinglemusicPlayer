69ded0379dd55dcb2f61cd6c423742c4288bf6b3
Piyush Kumar Prusty <piyushprusty10@gmail.com>
2021-06-15 19:25:09 +0530

    Media player using remote source Media player using local source

--- app/src/main/java/com/example/android/mediaplayer/MainActivity.java
+++ app/src/main/java/com/example/android/mediaplayer/MainActivity.java
@@ -7,6 +7,9 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.SeekBar;
+import android.widget.Toast;
+
+import java.io.IOException;
 
 public class MainActivity extends AppCompatActivity {
     private Button play;
@@ -18,20 +21,46 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        mediaPlayer = MediaPlayer.create(this, R.raw.music);
-        mediaPlayer.start();
+        play = findViewById(R.id.play);
+        pause = findViewById(R.id.pause);
 
+        //MediaPlayer using local source
+        mediaPlayer = MediaPlayer.create(this, R.raw.music);
+
+
+        //MediaPlayer using Remote Source
+        mediaPlayer = new MediaPlayer();
+        try {
+            mediaPlayer.setDataSource("http://penguinradio.dominican.edu/Sound%20FX%20Collection/Apple%20Bite.mp3");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        mediaPlayer.setOnPreparedListener(new MediaPlayer.OnPreparedListener() {
+            @Override
+            public void onPrepared(MediaPlayer mediaPlayer) {
+                Toast.makeText(MainActivity.this,"Ready To Play" , Toast.LENGTH_SHORT).show();
+                mediaPlayer.start();
+            }
+        });
+
+        mediaPlayer.prepareAsync();
+
+        //mediaPlayer.start();
         play.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+                mediaPlayer.start();
 
             }
         });
         pause.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-
+                mediaPlayer.pause();
             }
         });
+
+
     }
 }
